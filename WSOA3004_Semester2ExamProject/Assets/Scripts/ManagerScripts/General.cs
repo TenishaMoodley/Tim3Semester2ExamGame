a82840932b58acfd1035594734ecf1f8899e3a4c
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2022-01-27 11:46:14 +0200

    Connection Audio
    I can still hear  the audio looping, but  it's much  softer

--- WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/General.cs
+++ WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/General.cs
@@ -15,19 +15,21 @@ public class General : MonoBehaviour
 
     //Timer Additions//
     float time, waitingTime = 10;
-   
-
 
     public float Max_Distance;
+
+    public bool isApart = false;
+   
 
 
     private void Update()
     {
 
         float Distance = Vector3.Distance(Bean1_GO.transform.position, Bean2_GO.transform.position);
-        if (Distance > Max_Distance )//far apart from each other    and longer than specific amount of seconds. 
+        if (Distance > Max_Distance)//far apart from each other    and longer than specific amount of seconds. 
         {
-           
+            isApart = true;
+
             if (time < Time.time)
             {
                 Line_GO.SetActive(false);
@@ -38,17 +40,30 @@ public class General : MonoBehaviour
 
             }
 
+
             //Play Sound
             //FindObjectOfType<MusicManager>().Play("Connected");
-
+            
+           playSound();
         }
         else
         {
             Line_GO.SetActive(true);
             ctScript.stopTimer = true;
 
+
         }
     }
 
+    void playSound() 
+    {
+        if (isApart) 
+        {
+            FindObjectOfType<MusicManager>().Play("Connected");
+        }
+    }
+
+
+  
 
 }
